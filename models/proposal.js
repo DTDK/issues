baa373a949aa153d39f2c6d4f889a21c6960e6cf
Matt Megarry <mattmegarry@gmail.com>
2018-02-12 18:57:51 +0100

    layouts

--- models/proposal.js
+++ models/proposal.js
@@ -5,8 +5,17 @@ const ProposalSchema = Schema({
   name: String,
   text: String,
   status: String,
-  votes: [],
-  responses: []
+  votes:
+    {
+      for: Number,
+      against: Number
+    },
+  responses: [
+    {
+      body: String,
+      affirm: Boolean
+    }
+  ]
 });
 
 const Proposal = mongoose.model('Proposal', ProposalSchema);
