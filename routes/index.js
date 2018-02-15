101aae003a01f7d59669971ba0b6ac1c9d23d046
Daniel Thurdekoos <danielkoos@gmail.com>
2018-02-15 13:13:02 +0100

    styles login, new response

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'WE HAVE ISSUES' });
+  res.render('index', { title: 'WE GOT ISSUES' });
 });
 
 module.exports = router;
