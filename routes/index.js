cd3929d385179b84bd474874b5424968217e0a39
Matt Megarry <mattmegarry@gmail.com>
2018-02-15 13:13:06 +0100

    login and logout DONE, seed changed to create bcryted passwords

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  if (req.session.currentUser) {
+    return res.redirect('/proposals');
+  }
   res.render('index', { title: 'WE HAVE ISSUES' });
 });
 
