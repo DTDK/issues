568ff3f6d3d97f9df3787b05bdc862f2026c1edb
Matt Megarry <mattmegarry@gmail.com>
2018-02-16 12:13:00 +0100

    New Seed DONE

--- bin/seed.js
+++ bin/seed.js
@@ -25,76 +25,67 @@ const users = [
 
 const proposals = [
   {
-    name: 'Pizza Fridays',
-    text: 'Orci sagittis eu volutpat odio facilisis mauris sit amet. In hac habitasse platea dictumst quisque sagittis purus sit amet',
+    name: 'No more post-it note pranks',
+    text: 'Guys, I really think we should stop pranking the UX class with post-it notes through the glass. It\'s immature and makes us look like silly children.',
     status: 'pre-vote',
     votes:
       {
-        for: 6,
-        against: 3
+        for: 0,
+        against: 0
       },
-    responses: [
-      {
-        text: 'Yes! I love the taste of pizza',
-        category: 'positive'
-      },
-      {
-        text: 'Pizza makes you fat',
-        category: 'negative'
-      },
-      {
-        text: 'Pizza is much cheaper than our usual lunch choice',
-        category: 'positive'
-      }
-    ]
+    responses: []
   },
   {
-    name: 'Catalunya should be independent',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed euismod nisi porta lorem mollis aliquam. Purus semper eget duis at tellus at urna condimentum mattis. Elit at imperdiet dui accumsan. Ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis. Nisl nisi scelerisque eu ultrices vitae auctor eu. Aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod. Arcu non odio euismod lacinia at quis risus. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Id aliquet risus feugiat in ante. Pharetra convallis posuere morbi leo urna molestie at elementum. Feugiat in fermentum posuere urna nec. Sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. Cursus euismod quis viverra nibh cras pulvinar mattis nunc sed. Lectus nulla at volutpat diam ut venenatis tellus in. Consequat interdum varius sit amet mattis. Sed sed risus pretium quam vulputate.Velit euismod in pellentesque massa placerat duis.Luctus accumsan tortor posuere ac ut consequat semper viverra nam.Bibendum enim facilisis gravida neque convallis.Dignissim convallis aenean et tortor at risus viverra.Suscipit tellus mauris a diam.Tempor orci dapibus ultrices in.Iaculis nunc sed augue lacus viverra vitae.Velit scelerisque in dictum non consectetur a erat nam at.Egestas integer eget aliquet nibh praesent.Sed risus pretium quam vulputate dignissim suspendisse in est.Cum sociis natoque penatibus et magnis dis.Etiam sit amet nisl purus in mollis nunc sed id.Malesuada nunc vel risus commodo viverra maecenas.',
+    name: 'Ice Cream Machine',
+    text: 'If we had an ice cream machine then everyone would be happy all the time! Ice cream solves all you problems, and I like it.',
     status: 'voting-in-progress',
     votes:
       {
-        for: 50,
-        against: 50
-      },
-    responses: [
-      {
-        text: 'We will be more free without Spain!',
-        category: 'positive'
-      },
-      {
-        text: 'We won\'t be in the EU',
-        category: 'negative'
-      },
-      {
-        text: 'Pizza prices will rise if we leave',
-        category: 'negative'
-      }
-    ]
-  },
-  {
-    name: 'Compulsory water drinking',
-    text: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    status: 'accepted',
-    votes:
-      {
-        for: 9,
+        for: 0,
         against: 0
       },
     responses: [
       {
-        text: 'Water is nice',
+        text: 'No thanks Sheng, all our teeth will fall out!',
+        category: 'negative'
+      },
+      {
+        text: 'I heard those machines are only 30 euros - great idea!',
         category: 'positive'
       },
       {
-        text: 'Water is nasty',
-        category: 'negative'
+        text: 'Mmmmm, ice cream makes me happy!',
+        category: 'positive'
       }
     ]
   },
   {
-    name: 'Lets go to Mars on Thurday',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+    name: 'Schedule for Friday',
+    text: 'This Friday, we should do these sessions, in the following order: - Learn about Javascript Promises - In parallel: - Review doubts - Project work in pairs - Learn about industry culture (with beers!)',
+    status: 'accepted',
+    votes:
+      {
+        for: 17,
+        against: 3
+      },
+    responses: [
+      {
+        text: 'Culture is a good session to do at the end of the day when we are tired.',
+        category: 'positive'
+      },
+      {
+        text: 'People should just learn about Promises on their own - google it idiots!',
+        category: 'negative'
+      },
+      {
+        text: 'It would be good to make sure everyone knows promises before project week.',
+        category: 'positive'
+      }
+    ]
+  },
+  {
+    name: 'Giovanni Hummus',
+    text: 'Hi guys, I would like everyone to spread hummus on my body and watch me run around Ironhack on Friday night. It would be an important bonding experience and makes me really excited! Thanks, Gio xx',
     status: 'rejected',
     votes:
       {
@@ -103,15 +94,19 @@ const proposals = [
       },
     responses: [
       {
-        text: 'Yes! Mars is very fun',
-        category: 'positive'
-      },
-      {
-        text: 'We will die you idiots',
+        text: 'Haha - nice idea but I think I\'ll pass',
         category: 'negative'
       },
       {
-        text: 'Its impossible to get there',
+        text: 'You\'re disgusting',
+        category: 'negative'
+      },
+      {
+        text: 'Oh, what a great idea - we could all do with some \'release\' ;-)',
+        category: 'positive'
+      },
+      {
+        text: 'Giovanni, this is Adrià. Come see me in my office.',
         category: 'negative'
       }
     ]
